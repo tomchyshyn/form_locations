ba7ab95afd2c1e997acee10bebe5bd23207c85c5
Andriy Tomchyshyn <a.tomchyshyn@magneticone.com>
2014-10-20 07:17:46 +0300

    2

--- form_locations.php
+++ form_locations.php
@@ -22,11 +22,11 @@ function check_ready_state()
 
   $state = STATE_START;
 
-  if (isset_fields(array('db_host','db_user'))){
+  if (isset_fields(array('db_host','db_user', 'db_name'))){
     $state = STATE_DB_CONNECTION;
   }
 
-  if (isset_fields(array('db_name', 'tbl_country', 'col_country', 'col_country_iso2', 'col_country_iso3'))) {
+  if (isset_fields(array('tbl_country', 'col_country', 'col_country_iso2', 'col_country_iso3'))) {
     $state = STATE_DATA_SELECTION;
   }
 
@@ -63,12 +63,12 @@ function print_form($state)
 
   <form method="post">
 <?php
-  switch ($state) {
-    case STATE_START: form_fields_db_access();
-      break;
-    case STATE_DB_CONNECTION: form_fields_data_selection();
-      break;
-  }
+    switch ($state) {
+      case STATE_START: form_fields_db_access();
+        break;
+      case STATE_DB_CONNECTION: form_fields_data_selection();
+        break;
+    }
 ?>
     <button type="submit" name="submit-state-<?php echo $state; ?>" id="submit-state-<?php echo $state; ?>">submit</button>
   </form>
@@ -78,17 +78,24 @@ function print_form($state)
 function form_fields_db_access()
 {
 ?>
-  <p><span class="label required">host</span><input type="text" id="db_host" name="db_host" value="localhost"></p>
-  <p><span class="label required">user</span><input type="text" id="db_user" name="db_user" value="root"/></p>
-  <p><span class="label">pass</span><input type="password" id="db_pass" name="db_pass"/></p>
-  <p><span class="label">db name</span><input type="text" id="db_name" name="db_name"></p>
+  <h3>DB Access</h3>
+  <p><span class="label required">host</span><input type="text" id="db_host" name="db_host" value="localhost" required="required"></p>
+  <p><span class="label required">user</span><input type="text" id="db_user" name="db_user" value="root" required="required"></p>
+  <p><span class="label">pass</span><input type="password" id="db_pass" name="db_pass"></p>
+  <p><span class="label required">db name</span><input type="text" id="db_name" name="db_name" required="required"></p>
 <?php
 }
 
 function form_fields_data_selection()
 {
 ?>
-
+  <h3>Country</h3>
+  <p><span class="label required">table</span><input type="text" id="tbl_country" name="tbl_country" required="required"></p>
+  <p><span class="label">id</span><input type="text" id="col_country_id" name="col_country_id"></p>
+  <p><span class="label required">name</span><input type="text" id="col_country" name="col_country" required="required"></p>
+  <p><span class="label required">iso2</span><input type="text" id="col_country_iso2" name="col_country_iso2" required="required"></p>
+  <p><span class="label required">iso3</span><input type="text" id="col_country_iso3" name="col_country_iso3" required="required"></p>
+  <hr>
 <?php
 }
 
